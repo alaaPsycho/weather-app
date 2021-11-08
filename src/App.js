80ecad1fa40570144c9eba4bc83505bb702c3958
Tahar Alaa <alaaedine62@gmail.com>
2021-11-08 21:06:04 +0100

    weather

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+  const [temp,setTemp] = useState('');
+  const [desc,setDesc] = useState('');
+  const [city,setCity] = useState('algeria');
+  const [country,setCountry] = useState('');
+
+  const getData = (city,country)=>{
+
+    axios({
+      method :'GET',
+      url :`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=92449f23ccc46be861cd113396c23bdc`
+    }).then(
+      (response)=>{
+        console.log(response.data); setTemp(response.data.main.temp -273.15)
+        setDesc(response.data.weather[0].main)
+        setCountry(response.data.sys.country)
+      })
+    .catch((error)=>{console.log(error)})
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{margin:'auto'}}>
+      <div className='resault'>
+        <br/>
+        {city}      {country} 
+
+        <br/>
+        {Math.round(temp *100) /100 + '°C'.toLowerCase()} 
+        <br/>
+        {desc}
+        <br/>
+
+
+      </div>
+      <br/>
+        <h3 style={{textAlign:'center'}}>search countries or cities weather</h3>
+      <input onFocus={(e)=>{e.target.value =''}}  className='inp' value={city} onChange={(e)=>setCity(e.target.value)} />
+      <button className="btn" onClick={()=>{getData(city,country)}}>search weather</button>
     </div>
   );
 }
